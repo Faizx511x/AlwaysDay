0dd53976eed6d8b52a2662b4e061e734b3d519fb
FaizxDev <75949240+Faizx511x@users.noreply.github.com>
2022-04-04 14:24:52 +0300

    API3

---
+++ src/FaizDev/AlwaysDay/Main.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AlwaysDay;
+
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\Config;
+
+class Main extends PluginBase {
+	
+	public function onEnable(){
+		$this->getScheduler()->scheduleRepeatingTask(new UpdateTime($this), 20); 
+	}
+	
+}
