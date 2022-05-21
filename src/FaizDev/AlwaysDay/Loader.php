8a970560f02031c5c735e3e1b9873d7e247b9971
FaizxDev <75949240+Faizx511x@users.noreply.github.com>
2022-05-21 11:34:43 +0300

    DONE!

--- src/FaizDev/AlwaysDay/Loader.php
+++ src/FaizDev/AlwaysDay/Loader.php
@@ -5,10 +5,10 @@ declare(strict_types=1);
 namespace FaizDev\AlwaysDay;
 
 use pocketmine\plugin\PluginBase;
+
 use FaizDev\AlwaysDay\Tasks\SetDayTask;
 
-class Loader extends PluginBase
-{
+class Loader extends PluginBase{
 
     public function onEnable(): void
     {
