704382281c1bea2ff6a8f58d4206363d0ec31d68
FaizxDev <75949240+Faizx511x@users.noreply.github.com>
2022-05-21 11:20:15 +0300

    Fixed #1

---
+++ src/FaizDev/AlwaysDay/Tasks/SetDayTask.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FaizDev\AlwaysDay;
+
+use FaizDev\AlwaysDay\Loader;
+
+use pocketmine\scheduler\Task;
+use pocketmine\Server;
+use pocketmine\world\World;
+
+class SetDayTask extends Task
+{
+
+    public function onRun(): void
+    {
+        foreach(Server::getInstance()->getWorldManager()->getWorlds() as $worlds) {
+            $worlds->setTime(World::TIME_DAY);
+        }
+    }
+}
